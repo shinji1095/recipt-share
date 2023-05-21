f50fe08a87784e6bac078d355dc2bcf8cb0c1327
shinji1095 <shinji1095nameko@gmail.com>
2023-05-21 11:31:02 +0900

    first commit

---
+++ config.py
@@ -0,0 +1,3 @@
+# -------------------------- LINE BOT --------------------------
+CHANNEL_SECRET       = 'b174752cf51bb188d54cfdb6bf6a00c4'
+CHANNEL_ACCESS_TOKEN = 'W+aQdNW76WKZb68w0yIj8BfGyyyO6HqfDTOvtAr5ljoQLwc8tY5cNlvxWqksA6cCI/jCpPYxkA+YucWZXj9QcmWAjR+qC2Lwjdtxpl+TJrzDMq0ERSGnXgoO2nYbHhVGG1Y5onZQIx2BzGBwiT48nQdB04t89/1O/w1cDnyilFU='
