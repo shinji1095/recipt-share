91cf9babcfc03004b9b2371084e829c39ea588c4
shinji1095 <shinji1095nameko@gmail.com>
2023-05-21 12:29:31 +0900

    first commit

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ from config import (
     CHANNEL_ACCESS_TOKEN,
 )
 
-app = Flask('your_application')
+app = Flask(__name__)
 
 line_bot_api = LineBotApi(CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(CHANNEL_SECRET)
